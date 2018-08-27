9c121a843fd439c3dc42759a1e0bbaf0fe6566d9
Iurii Pigovskyi <yuriy.pigovsky@qvantel.com>
2018-08-27 11:43:54 +0300

    add ability to set custom headers for a request

--- src/main/js/com/github/pigovsky/rest_client_jsonpath_jq/model/HeaderFetcher.js
+++ src/main/js/com/github/pigovsky/rest_client_jsonpath_jq/model/HeaderFetcher.js
@@ -2,7 +2,12 @@ let HeaderFetcher = {
 	headers: function(url, onData) {
 		chrome.storage.sync.get("global", (data) => {
 			console.log("headers from config: " + JSON.stringify(data));
-			onData(data.global.headers.unconditional);
+			var jointHeaders = data.global.headers.unconditional;
+			var customHeaders = JSON.parse(document.getElementById("requestHeaders").value);
+			Object.keys(customHeaders).forEach( (key) => {
+				jointHeaders[key]=customHeaders[key];
+			});
+			onData(jointHeaders);
 		});
 	}
 };
