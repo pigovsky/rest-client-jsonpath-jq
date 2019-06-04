8809b7f1ee8a7d4915c46b159c156d9f16bd03f0
Jürij Pigowski <yuriy.pigovsky@qvantel.com>
2019-06-04 12:20:12 +0300

    Add saving content as a file method implementation

--- src/main/js/com/github/pigovsky/rest_client_jsonpath_jq/utils/FileUtils.js
+++ src/main/js/com/github/pigovsky/rest_client_jsonpath_jq/utils/FileUtils.js
@@ -1,10 +1,20 @@
 const FileUtils = {
   saveFileToClient: (filename, content) => {
-    throw "Not implemented";
+    const element = document.createElement('a');
+    const contentText = JSON.stringify(content, null, 2);
+    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(contentText));
+    element.setAttribute('download', filename);
+
+    element.style.display = 'none';
+    document.body.appendChild(element);
+
+    element.click();
+
+    document.body.removeChild(element);
   }
-}
+};
 
 module.exports = {
   FileUtils
-}
+};
 
